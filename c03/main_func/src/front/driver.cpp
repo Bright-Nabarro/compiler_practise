d3a8df45941374bd361fa9e5c25222684b48fa33
lin17 <lin17>
2024-11-21 17:47:22 +0800

    sync

--- c03/main_func/src/front/driver.cpp
+++ c03/main_func/src/front/driver.cpp
@@ -1,46 +1,74 @@
 #include "driver.hpp"
 
-#include <print>
+#include <format>
+#include <llvm/Support/WithColor.h>
 #include "file_manager.hpp"
 
 namespace tinyc
 {
 
-Driver::Driver():
-	m_file_manager { std::make_shared<FileManager>() },
-	m_ast { nullptr },
-	m_location {},
-	m_debug_trace { false }
-{}
-
-auto Driver::parse(std::string_view file_name) -> bool
+Driver::Driver(llvm::SourceMgr& src_mgr):
+	m_ast {},
+	m_src_mgr { src_mgr },
+	m_bufferid {},
+	m_debug_trace { false },
+	m_parser {}
 {
-	auto handle = m_file_manager->read_file(file_name);
-	if (!handle)
-	{
-		std::println(stderr, "{}", handle.error());
-		return false;
-	}
-	m_location.initialize(&file_name);
-	set_flex(handle.value());
-
-	yy::parser parse(*this);
-	parse.set_debug_level(this->get_trace());
-	int parse_ret = parse();
-
-	m_file_manager->close_file(handle.value());
-
-	if (parse_ret != 0)
-	{
-		return false;
-	}
-
-	return true;
+	
 }
 
-void report_error(yy::parser::location_type loc, const std::string& msg)
+auto Driver::construct(std::string_view file_name)
+	-> std::expected<void, std::string>
 {
-	std::cerr<<loc<<": "<<msg.data()<<std::endl;
+	auto buffer_or_error = llvm::MemoryBuffer::getFile(file_name);
+	if (!buffer_or_error)
+	{
+		return std::unexpected{std::format("Failed to open {} \n", file_name)};
+	}
+	m_bufferid = m_src_mgr.AddNewSourceBuffer(std::move(*buffer_or_error), llvm::SMLoc());
+
+	m_parser = std::make_unique<yy::parser>(*this);
+
+	return {};
+}
+
+auto Driver::get_buffer() -> const char*
+{
+	return m_src_mgr.getMemoryBuffer(m_bufferid)->getBufferStart();
+}
+
+//auto Driver::parse(std::string_view file_name) -> bool
+//{
+//	auto handle = m_file_manager->read_file(file_name);
+//	if (!handle)
+//	{
+//		std::println(stderr, "{}", handle.error());
+//		return false;
+//	}
+//	m_location.initialize(&file_name);
+//	set_flex(handle.value());
+//
+//	yy::parser parse(*this);
+//	parse.set_debug_level(this->get_trace());
+//	int parse_ret = parse();
+//
+//	m_file_manager->close_file(handle.value());
+//
+//	if (parse_ret != 0)
+//	{
+//		return false;
+//	}
+//
+//	return true;
+//}
+
+auto Driver::parse() -> bool
+{
+	m_parser->set_debug_level(this->get_trace());
+	int parse_ret = (*m_parser)();
+
+	return parse_ret == 0;
+	
 }
 
 }	//namespace tinyc
--- c03/main_func/src/front/include/driver.hpp
+++ c03/main_func/src/front/include/driver.hpp
@@ -1,7 +1,10 @@
 #pragma once
 
 #include <llvm/Support/SourceMgr.h>
+#include <llvm/Support/SMLoc.h>
+#include <llvm/Support/MemoryBuffer.h>
 #include <string_view>
+#include <expected>
 #include <memory>
 #include "ast.hpp"
 #include "bison_parser.hpp"
@@ -15,15 +18,16 @@ YY_DECL;
 namespace tinyc
 {
 
-void report_error(yy::parser::location_type loc, const std::string& m);
-
 class Driver
 {
+	friend class DriverFactory;
+private:
+	Driver(llvm::SourceMgr& src_mgr);
+
 public:
-	Driver();
-	auto parse(std::string_view file_name) -> bool;
-	auto get_location() -> yy::location&
-	{ return m_location; }
+	auto construct(std::string_view file_name)
+		-> std::expected<void, std::string>;
+	auto parse() -> bool;
 	
 	//CompUnit为根节点
 	void set_ast(std::unique_ptr<CompUnit> ast)
@@ -39,17 +43,34 @@ public:
 	auto get_trace() -> bool
 	{ return m_debug_trace; }
 	
-	void flex_fatal_error(std::string_view msg);
+	void report_error(const char* buf_pos);
 
 private:
-	void set_flex(std::FILE* handle);
+	auto get_buffer() -> const char*;
 
 private:
-	std::shared_ptr<FileManager> m_file_manager;
 	std::unique_ptr<CompUnit> m_ast;
-	yy::location m_location;
+	llvm::SourceMgr& m_src_mgr;
+	unsigned m_bufferid;
 	bool m_debug_trace;
+	std::unique_ptr<yy::parser> m_parser;
 };
+
+
+class DriverFactory
+{
+public:
+	DriverFactory(llvm::SourceMgr& src_mgr):
+		m_src_mgr { src_mgr }
+	{}
+
+	auto produce_driver(std::string_view file_name)
+		-> std::expected<Driver, std::string>;
+
+private:
+	llvm::SourceMgr& m_src_mgr;
+};
+
 
 }	//namespace tinyc
 
